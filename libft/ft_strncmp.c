a826f330de9a2f7c7d6f774d0ecff2ec7e917b03
Drew Pentland <drewtpentland@gmail.com>
2022-11-15 22:03:24 +0100

    Finished strncmp. saving and saving moulinette py

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -19,5 +19,8 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
     while (s1[i] == s2[i] && s1[i] != '\0' && i < n)
         i++;
-    return (s1[i] - s2[i]);
+    if (i == n)
+        return (0);
+    else
+        return (s1[i] - s2[i]);
 }
