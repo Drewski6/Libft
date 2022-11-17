9450f19e972736c1042ad77805078be86abcc76e
Drew Pentland <dpentlan@student.42.fr>
2022-11-17 17:47:09 +0100

    Saving because I'm leaving. Mid trim.

--- libft/libft.h
+++ libft/libft.h
@@ -42,5 +42,6 @@ void	*ft_calloc(size_t num_elem, size_t elem_size);
 char	*ft_strdup(const char *src);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 
 #endif
