48fe68b6ff504f0fc5463f4accbdd8c99a2a63e2
Drew Pentland <dpentlan@student.42.fr>
2022-11-17 13:44:26 +0100

    strdup finished. Thanks for playing

--- libft/libft.h
+++ libft/libft.h
@@ -39,5 +39,6 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int	ft_atoi(const char *nptr);
 void	*ft_calloc(size_t num_elem, size_t elem_size);
+char	*ft_strdup(const char *src);
 
 #endif
