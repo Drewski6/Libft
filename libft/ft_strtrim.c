c20e0e2d02d895012dc6c7f1ed1c1bf11409650e
Drew Pentland <drewtpentland@gmail.com>
2022-11-19 20:39:12 +0100

    Staring split. saving progress for now.

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -10,10 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "libft.h"
+
+int	ft_in_charset(int c, char *set)
+{
+	while (*set)
+	{
+		if (*set == (unsigned char)c)
+			return (1);
+		set++;
+	}
+	return (0);
+}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	size_t	i;
+	size_t	j;
+	char	*ptr;
+	size_t	s1_len;
 
-	
+	i = 0;
+	j = 0;
+	s1_len = ft_strlen(s1);
+	while (ft_in_charset(*((char *)s1 + i), (char *)set))
+		i++;
+	while (ft_in_charset(*((char *)s1 + (s1_len - 1 - j)), (char *)set))
+		j++;
+	ptr = ft_substr(s1, i, (s1_len - (i + j)));
+	return (ptr);
 }
