1853cf8be17003d6e9e1148981ce9ce243f37835
Drew Pentland <drewtpentland@gmail.com>
2022-11-25 14:33:40 +0100

    fixed memcmp

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -19,11 +19,12 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	if (n == 0)
 		return (0);
 	i = 0;
-	while (*((unsigned char *)s1 + i) == *((unsigned char *)s2 + i) && i < n)
+	while (*((unsigned char *)s1 + i) == *((unsigned char *)s2 + i) && i < n - 1)
 	{
 		i++;
 	}
 	if (i == n)
 		return (0);
-	return (*((unsigned char *)s1 + i) - *((unsigned char *)s2 + i));
+	else
+		return (*((unsigned char *)s1 + i) - *((unsigned char *)s2 + i));
 }
