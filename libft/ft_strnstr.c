5a61af947ae27e6d52f8bb644c2c7901a73166d4
Drew Pentland <drewtpentland@gmail.com>
2022-11-26 22:17:29 +0100

    fixed strnstr.

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -18,7 +18,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	j;
 
 	i = 0;
-	if (*little == 0 && len == 0)
+	if (*little == 0 || big == 0)
 		return ((char *)big);
 	while (big[i] && i < len)
 	{
