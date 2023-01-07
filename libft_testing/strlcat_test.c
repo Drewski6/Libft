1753cedc7f24e33a37d204d3a0eb0ca55513ff58
Drew Pentland <dpentlan@student.42.fr>
2023-01-07 11:44:39 +0100

    Couple of changes. Final touches prior to push.

--- libft_testing/strlcat_test.c
+++ libft_testing/strlcat_test.c
@@ -4,25 +4,11 @@
 #include <bsd/string.h>
 #include "../libft/libft.h"
 
-int main (int argc, char* argv[])
+int main (void)
 {
-    char string_src[50] = "Hi this is a source string.";
-    char string_dest[50]= "";
-    int size;
-    int return_val;
+	char	*dest;
 
-    size = atoi(argv[2]);
-    if (atoi(argv[1]) == 0)
-    {
-        //printf("strlcpy,before,%d,%s\n",size, string_dest);
-        return_val = strlcat(string_dest, string_src, size);
-        printf("strlcpy,after,%d,%s,%d\n",size, string_dest, return_val);
-    }
-    else if (atoi(argv[1]) == 1)
-    {
-        //printf("ft_strlcpy,before,%d,%s\n",size, string1_dest);
-        return_val = ft_strlcat(string_dest, string_src, size);
-        printf("ft_strlcpy,after,%d,%s,%d\n",size, string_dest, return_val);
-    }
-    return (0);
+	dest = (char *)malloc(sizeof(char) * 100);
+	printf("%ld", strlcat(dest, "lorem ipsum dolor sit amet", 5));
+	return 0;
 }
