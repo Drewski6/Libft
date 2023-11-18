541d4e8b39354cc139861d646190cfac78d0dcc8
Drew Pentland <drewtpentland@gmail.com>
2023-11-18 15:28:10 +0100

    Added some formatting for ft_print_table

--- srcs/custom/ft_print_table.c
+++ srcs/custom/ft_print_table.c
@@ -6,36 +6,55 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 13:45:22 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/11/18 13:46:52 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/11/18 15:27:43 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static void	ft_print_tab_flag_0(char **table)
+{
+	int	i;
+
+	i = 0;
+	ft_printf("\n");
+	while (table[i])
+		ft_printf("%s\n", table[i++]);
+	return ;
+}
+
+static void	ft_print_tab_flag_1(char **table)
+{
+	int	i;
+
+	i = 0;
+	ft_printf("\n[\"%s\"", table[i++]);
+	while (table[i])
+		ft_printf(",\"%s\"", table[i++]);
+	ft_printf("]\n");
+	return ;
+}
 
 /*
 **	NAME
 		ft_print_table
 **	DESCRIPTION
 		Prints the contents of a table by printing each address in a char **.
+		A flag can be set to change how the table formatting is done.
 **	RETURN
 		Void function does not return a value.
 */
 
-void	ft_print_table(char **table)
+void	ft_print_table(char **table, int flag)
 {
-	int	i;
-
-	i = 0;
 	if (!table)
 	{
 		ft_printf("(NULL)\n");
 		return ;
 	}
-	if (table)
-	{
-		ft_printf("\n");
-		while (table[i])
-			ft_printf("%s\n", table[i++]);
-	}
+	if (flag == 0)
+		ft_print_tab_flag_0(table);
+	if (flag == 1)
+		ft_print_tab_flag_1(table);
 	return ;
 }
