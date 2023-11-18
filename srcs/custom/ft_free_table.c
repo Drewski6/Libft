250ae6e6b7c158ba70e7465a4e688536c18e8ab5
Drew Pentland <drewtpentland@gmail.com>
2023-11-18 19:10:22 +0100

    Adding comment to ft_table_free so see if it works in lsp

--- srcs/custom/ft_free_table.c
+++ srcs/custom/ft_free_table.c
@@ -6,20 +6,20 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 13:49:36 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/11/18 17:46:28 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/11/18 19:10:11 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
 /*
-**	NAME
-**		ft_table_free
-**	DESCRIPTION
-**		Frees a table pointed to by char **table.
-**	RETURN
-**		Void function does not return a value.
-*/
+ *	NAME
+ *		ft_table_free
+ *	DESCRIPTION
+ *		Frees a table pointed to by char **table.
+ *	RETURN
+ *		Void function does not return a value.
+ */
 
 void	ft_table_free(char **table)
 {
