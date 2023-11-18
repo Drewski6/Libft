5439574ee60f82269a854e69f0c848e7fd233687
Drew Pentland <drewtpentland@gmail.com>
2023-11-18 16:48:07 +0100

    Adding helper function for ft_striteri

---
+++ srcs/custom/ft_newline_to_null.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_newline_to_null.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/18 16:47:11 by dpentlan          #+#    #+#             */
+/*   Updated: 2023/11/18 16:48:01 by dpentlan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+**	NAME
+		ft_newline_to_null
+**	DESCRIPTION
+		To be used with ft_striteri.
+		Replaces any newline character at the end of a line with null.
+**	RETURN
+		Void function does not return a value.
+*/
+
+void	ft_newline_to_null(unsigned int i, char *ch_addr)
+{
+	(void) i;
+	if (*ch_addr == '\n' && *(ch_addr + 1) == '\0')
+		*ch_addr = 0;
+	return ;
+}
