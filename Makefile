aa6d0cf662a67790f6e4fdee46134538c62eb0a3
Drew Pentland <drewtpentland@gmail.com>
2023-11-19 22:59:22 +0100

    Added function to find the largest size in a linked list

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/11/07 14:36:56 by dpentlan          #+#    #+#              #
-#    Updated: 2023/11/19 21:53:50 by dpentlan         ###   ########.fr        #
+#    Updated: 2023/11/19 22:59:10 by dpentlan         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -71,6 +71,7 @@ SRCS =				mandatory/ft_isalpha.c \
 					custom/ft_lst/ft_lst_free_link.c \
 					custom/ft_lst/ft_lst_print_link.c \
 					custom/ft_lst/ft_lst_print_link_nl.c \
+					custom/ft_lst/ft_lst_largest.c \
 					custom/ft_print_table.c \
 					custom/ft_free_table.c \
 					custom/ft_newline_to_null.c \
--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 14:31:39 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/11/19 21:53:32 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/11/19 22:58:16 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,6 +80,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 void	ft_lst_free_link(void *content);
 void	ft_lst_print_link(void *content);
 void	ft_lst_print_link_nl(void *content);
+size_t	ft_lst_largest(t_list **lst, size_t (*s)(void *));
 void	ft_free_table(char **table);
 void	ft_newline_to_null(unsigned int i, char *ch_addr);
 void	ft_print_table(char **table, int flag);
